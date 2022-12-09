4f05672a86f88d9e66381eda04d51adfc6da3fa1
Angelo <angelomendoza671@gmail.com>
2022-12-09 10:50:38 -0600

    Encriptador finalizado

---
+++ script.js
@@ -0,0 +1,52 @@
+const inputText = document.querySelector(".input-primerTexto") //constante document.querySelector para traer lo de html a java
+const mensaje = document.querySelector(".mensaje")
+
+//La letra "e" es convertida para "enter"
+//La letra "i" es convertida para "imes"
+//La letra "a" es convertida para "ai"
+//La letra "o" es convertida para "ober"
+//La letra "u" es convertida para "ufat"
+
+function btnEncriptar(){
+    const textoEncriptado = encriptar(inputText.value)
+    mensaje.value = textoEncriptado;
+    mensaje.style.backgroundImage = "none"
+    inputTexto.value =""
+}
+
+function encriptar(stringEncriptada){
+    let matrizCodigo = [["e", "enter"],["i","imes"],["a", "ai"], ["o", "ober"], ["u","ufat"]]; //las listas se marcan con corchetes dos listas dos corchetes     
+    stringEncriptada = stringEncriptada.toLowerCase(); //Convertir en minuscula
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringEncriptada.includes(matrizCodigo[i][0])){
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0],matrizCodigo[i][1])
+        }
+    }
+    return stringEncriptada;
+}
+
+function btnDesencriptar(){
+    const textoEncriptado = desencriptar(inputText.value)
+    mensaje.value = textoEncriptado;
+    mensaje.style.backgroundImage = "none"
+    inputTexto.value =""
+}
+
+function desencriptar(stringDesencriptada){
+    let matrizCodigo = [["e", "enter"],["i","imes"],["a", "ai"], ["o", "ober"], ["u","ufat"]]; //las listas se marcan con corchetes dos listas dos corchetes     
+    stringDesencriptada = stringDesencriptada.toLowerCase(); //Convertir en minuscula
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringDesencriptada.includes(matrizCodigo[i][1])){
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1],matrizCodigo[i][0])
+        }
+    }
+    return stringDesencriptada;
+}
+
+function copiar(){
+    mensaje.select()
+    navigator.clipboard.writeText(mensaje.value)
+    mensaje.value=""
+}
